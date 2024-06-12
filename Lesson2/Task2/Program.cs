db11f93f8e56460686733a9d875a1332b4070cf5
GeorgeKleinos <kleinos@inbox.ru>
2024-06-12 17:00:36 +0300

    add all Lesson2:Tasks

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+// while (i < n)  Можно пользоваться как переменной n, так и свойством Length(Длинна массива arr).
+
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    // Console.Write(arr[i]);
+    // Console.Write(' ');
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+
+}
